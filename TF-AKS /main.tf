ddd24d939391c18ef3034f3bb46e3341e68f8e84
vijayGaneshBhavaraju <95146865+vijayGaneshBhavaraju@users.noreply.github.com>
2025-07-03 00:16:48 +0000

    Create main.tf

---
+++ TF-AKS /main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.35.0"
+    }
+  }
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = var.rg_name
+  location            = var.rg_location
+  sku                 = "Premium"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "akscluster" {
+  name                = var.aks_name
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  dns_prefix          = "nextopsaks-dev"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_B2s"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "calico"
+    dns_service_ip = "10.0.0.10"
+    service_cidr = "10.0.0.0/16"
+    load_balancer_sku = "standard"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "aks2acr" {
+  principal_id                     = azurerm_kubernetes_cluster.akscluster.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+#  set this SQL server name nad credentials in the ADO Variable group 
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                = "xxxxxxxxxx"
+  resource_group_name = var.rg_name
+  location            = var.rg_location
+  version             = "12.0"
+  administrator_login          = "xxxxxxxxxx"
+  administrator_login_password = "xxxxxxxxxx"
+}
+
+resource "azurerm_mssql_database" "sqldb" {
+  name                = "mhcdb"
+  server_id           = azurerm_mssql_server.sqlserver.id
+  collation           = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb         = 1
+  sku_name            = "Basic"
+}
